c9cec2be639980167c2a5dfd5dcde59a96dd5c65
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-17 09:50:06 -0300

    Tipo any

--- 02-tipagem-basica.ts
+++ 02-tipagem-basica.ts
@@ -16,3 +16,12 @@ console.log(`${nome} agora tem ${idade} anos.`);
 console.log(`Status: ${ativo ? "Ativo" : "Inativo"}`);
 
 separador();
+
+/* Sobre o tipo any (qualquer) 
+Tipo neutro, usado SOMENTE em situações de exceção em que não é 
+possível rapidamente identificar qual o tipo do valor (exemplo: refatoração complexa de um projeto com JS antigo e que deve migrar pra TS). */
+let qualquerCoisa: any = "Pode ser qualquer coisa";
+qualquerCoisa = 10; // number
+qualquerCoisa = true; // agora boolean
+
+console.log(`Qualquer coisa: ${qualquerCoisa}`);
