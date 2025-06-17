a1c0bca55bb44eede932a085bcd608d8fd795e1b
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-17 11:46:15 -0300

    Enunciado

---
+++ exercicio01.ts
@@ -0,0 +1,12 @@
+// exercicio01.ts
+
+/* 
+1) Crie uma função chamada "exibirCor" que aceite:
+- nome da pessoa (obrigatório)
+- cor preferida da pessoa (opcional)
+
+Atenção: o parâmetro cor deve ser do tipo Cor e aceitar somente 5 cores.
+
+2) Chame a função pelo menos 2x:
+- Na primeira, passe o nome e uma cor (das possíveis)
+- Na segunda, passe somente o nome */
