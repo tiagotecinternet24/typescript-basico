9995bcd9b59975743b8fb58b3a86296efd0194aa
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-18 11:37:30 -0300

    Create exercicio02.ts

---
+++ exercicio02.ts
@@ -0,0 +1,10 @@
+// exercicio02.ts
+
+/* 1) Crie uma interface para representar um produto com 
+as propriedades nome, preço e garantia. */
+
+/* 2) Cria uma função que receba um objeto baseado na interface Produto 
+e que mostre nome, preço e garantia. Obs.: o objeto deve ser 
+desestruturado como parâmetros individuais. */
+
+/* 3) Use a função pelo menos 2x passando produtos diferentes. */
