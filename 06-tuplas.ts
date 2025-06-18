0fd988d8686a31127b993340121c0c76a313ef89
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-18 09:43:41 -0300

    Aplica tupla em retorno de função

--- 06-tuplas.ts
+++ 06-tuplas.ts
@@ -1,3 +1,5 @@
+import { separador } from "./modulos";
+
 // 06-tuplas.ts
 export {};
 
@@ -11,3 +13,28 @@ console.log(usuario[2] ? "ativo" : "inativo");
 
 usuario[1] = 30; // atualizando um valor da tupla (desde que não seja readonly)
 console.log(usuario[1] + " anos.");
+
+separador();
+
+// Criando uma tupla como tipo
+type Produto = [string, number];
+
+const produtoA: Produto = ["Camiseta", 29.99];
+//const produtoB:Produto = [100, "Calça"]; // erro, pois está fora do padrão da tupla
+const produtoC: Produto = ["TV", 1000];
+
+console.log(produtoA);
+console.log(produtoC);
+
+separador();
+
+// Usando tupla no retorno de função
+function verificarOperacao(sucesso: boolean): [number, string] {
+  if (sucesso) {
+    return [200, "Operação realizada com sucesso!"];
+  }
+  return [500, "Erro inesperado no servidor"];
+}
+
+let resultadoDaOperacao = verificarOperacao(false);
+console.log(resultadoDaOperacao);
--- dist/06-tuplas.js
+++ dist/06-tuplas.js
@@ -1,8 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
 const usuario = ["João", 12, true];
 console.log(usuario[0]);
 console.log(usuario[1] + " anos");
 console.log(usuario[2] ? "ativo" : "inativo");
 usuario[1] = 30;
 console.log(usuario[1] + " anos.");
+(0, modulos_1.separador)();
+const produtoA = ["Camiseta", 29.99];
+const produtoC = ["TV", 1000];
+console.log(produtoA);
+console.log(produtoC);
+(0, modulos_1.separador)();
+function verificarOperacao(sucesso) {
+    if (sucesso) {
+        return [200, "Operação realizada com sucesso!"];
+    }
+    return [500, "Erro inesperado no servidor"];
+}
+let resultadoDaOperacao = verificarOperacao(false);
+console.log(resultadoDaOperacao);
