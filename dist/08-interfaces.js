143c7c2804aa26f4fb63573c1289c5e90bc0799f
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-18 11:37:25 -0300

    Create 08-interfaces.js

---
+++ dist/08-interfaces.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const usuarioA = {
+    nome: "João",
+    idade: 25,
+    ativo: true,
+};
+const usuarioB = {
+    nome: "Alana",
+    idade: 17,
+    ativo: false,
+    email: "alana@gmail.com",
+};
+console.log(usuarioA);
+console.log(usuarioB);
