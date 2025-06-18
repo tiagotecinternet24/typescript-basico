448e1615598eab4974db2fe8bae1fbff79173414
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-18 08:42:42 -0300

    Create 05-arrays.js

---
+++ dist/05-arrays.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
+const bandas = ["Metallica", "Slayer", "Iron Maiden"];
+bandas.push("Nightwish");
+console.log(bandas);
+const artistas = ["Jon Oliva", "Sting", "David Gilmour"];
+artistas.push("Neil Peart");
+console.log(artistas);
+(0, modulos_1.separador)();
+const pessoas = ["Tiago", 28, "Maria", 45];
+console.log(pessoas);
+(0, modulos_1.separador)();
+const linguagens = [
+    ["JS", "TS"],
+    ["HTML", "CSS", "Bootstrap"],
+];
+console.log(linguagens[0][0]);
+console.log(linguagens[1][0]);
+(0, modulos_1.separador)();
